e8e0dcb2c1d3033addf091eae80c3dc41a9cf994
lishuo <my_ls@icloud.com>
2025-07-24 09:35:05 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,25 +25,25 @@ export default function RootLayout({
 }) {
   return (
     <html lang="zh-CN" suppressHydrationWarning>
-      <head>
-        {/* Google Analytics */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-QG9PGG4K13"
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-QG9PGG4K13');
-            `,
-          }}
-        />
-      </head>
+      {/*<head>*/}
+      {/*  /!* Google Analytics *!/*/}
+      {/*  <Script*/}
+      {/*    strategy="afterInteractive"*/}
+      {/*    src="https://www.googletagmanager.com/gtag/js?id=G-QG9PGG4K13"*/}
+      {/*  />*/}
+      {/*  <Script*/}
+      {/*    id="google-analytics"*/}
+      {/*    strategy="afterInteractive"*/}
+      {/*    dangerouslySetInnerHTML={{*/}
+      {/*      __html: `*/}
+      {/*        window.dataLayer = window.dataLayer || [];*/}
+      {/*        function gtag(){dataLayer.push(arguments);}*/}
+      {/*        gtag('js', new Date());*/}
+      {/*        gtag('config', 'G-QG9PGG4K13');*/}
+      {/*      `,*/}
+      {/*    }}*/}
+      {/*  />*/}
+      {/*</head>*/}
       <body suppressHydrationWarning>
         <ThemeProvider
           attribute="class"
